968cd74b150404acb1c2b9f48ca7263c79a872a2
Khang <khangda01808@gmail.com>
2026-04-05 02:44:35 +0000

    update side bar

--- src/components/UserDetail/index.jsx
+++ src/components/UserDetail/index.jsx
@@ -1,24 +1,29 @@
-import React from "react";
-import {Typography} from "@mui/material";
+import React, { useState } from "react";
+import { Typography } from "@mui/material";
 
 import "./styles.css";
-import {useParams} from "react-router-dom";
+import { useParams } from "react-router-dom";
+import models from "../../modelData/models";
 
 /**
  * Define UserDetail, a React component of Project 4.
  */
 function UserDetail() {
-    const user = useParams();
-    return (
-        <>
-          <Typography variant="body1">
+  const [users, setUsers] = useState(models.userListModel());
+  console.log(users);
+  const user = useParams();
+  console.log(models.userModel(user));
+  return (
+    <>
+      {/* <Typography variant="body1">
             This should be the UserDetail view of the PhotoShare app. Since it is
             invoked from React Router the params from the route will be in property match.
             So this should show details of user: {user.userId}.
             You can fetch the model for the user from models.userModel.
-          </Typography>
-        </>
-    );
+          </Typography> */}
+      <h1></h1>
+    </>
+  );
 }
 
 export default UserDetail;
--- src/components/UserList/index.jsx
+++ src/components/UserList/index.jsx
@@ -6,6 +6,7 @@ import {
   ListItemText,
   Typography,
 } from "@mui/material";
+import { Link } from "react-router-dom";
 
 import "./styles.css";
 import models from "../../modelData/models";
@@ -13,31 +14,37 @@ import models from "../../modelData/models";
 /**
  * Define UserList, a React component of Project 4.
  */
-function UserList () {
-    const users = models.userListModel();
-    return (
-      <div>
-        <Typography variant="body1">
+function UserList() {
+  const users = models.userListModel();
+  return (
+    <div>
+      {/* <Typography variant="body1">
           This is the user list, which takes up 3/12 of the window. You might
           choose to use <a href="https://mui.com/components/lists/">Lists</a>{" "}
           and <a href="https://mui.com/components/dividers/">Dividers</a> to
           display your users like so:
-        </Typography>
-        <List component="nav">
-          {users.map((item) => (
-            <>
-              <ListItem>
-                      <ListItemText primary={item.first_name}/>
-              </ListItem>
-              <Divider />
-            </>
-          ))}
-        </List>
-        <Typography variant="body1">
-          The model comes in from models.userListModel()
-        </Typography>
-      </div>
-    );
+        </Typography> */}
+      <List component="nav">
+        {users.map((user) => (
+          <>
+            <ListItem
+              to={`/users/${user._id}`}
+              component={Link}
+              key={user._id}
+              divider
+              button
+            >
+              <ListItemText primary={user.first_name} />
+            </ListItem>
+            <Divider />
+          </>
+        ))}
+      </List>
+      {/* <Typography variant="body1">
+        The model comes in from models.userListModel()
+      </Typography> */}
+    </div>
+  );
 }
 
 export default UserList;
